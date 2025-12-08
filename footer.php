ff21a90393d9b626b0399b3dfe95b64da00e5631
Patyshaaa <patricia.busch@student.isfsc.be>
2025-12-08 16:42:26 +0100

    add br tag

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <div style="text-align:center;">
+    <img src="copyright.png" alt="Copyright" style="width:20px; vertical-align:middle;">
+    <span><?php echo date('Y'); ?> BeBeats. <br>Tous droits réservés.</span>
+  </div>
+</footer>
