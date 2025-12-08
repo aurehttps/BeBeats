8e924a20bdb11a35230413980e3ea4acc9a9edd1
aurehttps <aurelienremacle@gmail.com>
2025-12-08 14:43:34 +0100

    Changement de nom pour WP

---
+++ front-page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="" alt="logo bebeats">
+        <p>Chercher dans BeBeats...</p>
+    </header>
+    
+    <section class="Menu gauche">
+        <ul>
+            <li> <a href="#"></a>Accueil</li>
+            <li><a href="#"></a>Recherche</li>
+            <li><a href="#"></a>Contribuer</li>
+            <li><a href="#"></a>Events</li>
+            <li><a href="#"></a>Réglages</li>
+        </ul>
+    </section>
+
+    <section class="Accueil SM">
+        <ul>
+            <li><a href="#"></a>Accueil</li>
+            <li><a href="#"></a>Feed</li>
+        </ul>
+    </section>
+
+    <section class="Vidéo">
+        <video width="" height="">
+            <source src="" type="">
+        </video>
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
