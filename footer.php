bc861b996803403668d3c783d070404a714ecfdf
aurehttps <aurelienremacle@gmail.com>
2026-01-03 19:40:05 +0100

    suite

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
-<footer>
-  <div>
-    &copy; <?php echo date('Y'); ?> BeBeats. Tout droits réservés.
-  </div>
-</footer>
+    <?php wp_footer(); ?>
+    
+    <footer>
+      <div>
+        &copy; <?php echo date('Y'); ?> BeBeats. Tout droits réservés.
+      </div>
+    </footer>
+</body>
+</html>
